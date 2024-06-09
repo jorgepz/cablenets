8df373a895e4338b963e8b4cad415c8933d4bf52
jorgepz <jorge.jmpz@gmail.com>
2024-06-09 07:54:44 -0300

    add test

--- tests/test_1.py
+++ tests/test_1.py
@@ -19,7 +19,6 @@ disp_mat = np.array([ [ 0, 0   , 0, 0   ],
 fext_mat = np.array([[ 1, 1, 0.0, 0]]) # node fx fy fz
 
 nodes_def, normal_forces = cn.solve( nodes, connec, ks_vec, disp_mat, fext_mat )
-print("toy adentro")
 
 def test_normal_force():
-    assert abs(normal_forces[1]) < 1e-7
+    assert ( abs(normal_forces[1]) < 1e-7) and ( abs(normal_forces[0]-1)<1e-7 )
