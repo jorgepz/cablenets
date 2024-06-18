7e7e9b26f7cfaf94f847fbca822b0c7a3c9dd0e4
jorgepz <jorge.jmpz@gmail.com>
2024-06-18 12:16:30 -0300

    editing example

--- examples/saddle.py
+++ examples/saddle.py
@@ -18,17 +18,18 @@ nn  = 3 # number nodes per edge
 E   = 1 # young modulus
 A   = 1 # cross-section area
 
-nc = nnodes-1 # number cells per edge
+nc = nn-1 # number cells per edge
 
-nelems = nc*nc*4 + nc*2
+nelems = nc*nc*2 + nc*4
 nnodes = nn*nn
 
 # node coordinates matrix
 nodes  = np.zeros((nnodes,3))
-for i in range(nn):
-    for j in range(nn):
-        ind_node = i*nn
-        nodes[ ind_node, :] = [i*L/nelems, 0.0, 0.0]
+for j in range(nn):
+    for i in range(nn):
+        ind_node = i + j*nn
+        print("node: ", ind_node)
+        nodes[ ind_node, :] = [i*L/nc, j*L/nc, 0.0]
 
 # young and area vectors
 youngs = np.array([ E ])
@@ -36,16 +37,33 @@ areas  = np.array([ A ])
 
 # connectivity matrix
 connec = np.zeros((nelems,4),dtype=int)
-for i in range(nelems):
-    connec[i,:] = [0, 0, i, i+1 ]
+# cross cables
+for j in range(nc):
+    for i in range(nc):
+        ind_cell = i + j*nc
+        print("ind cell", ind_cell, " i ", i, " j ", j)
+        connec[ind_cell*2  , : ] = [0, 0, j*nn+i  , (j+1)*nn+i+1 ]
+        connec[ind_cell*2+1, : ] = [0, 0, j*nn+i+1, (j+1)*nn+i   ]
+ind_cell = 2*nc*nc-1
+for i in range(nc):
+    print("i", i)
+    ind_cell = ind_cell+1
+    print("ind_cell", ind_cell)
+    connec[ind_cell  , : ] = [0, 0, i  , i+1 ]
+    ind_cell = ind_cell+1
+    connec[ind_cell  , : ] = [0, 0, i+ nn*(nn-1)  , i+1+nn*(nn-1)]
+    ind_cell = ind_cell+1
+    connec[ind_cell  , : ] = [0, 0, i*nn  , (i+1)*nn ]
+    ind_cell = ind_cell+1
+    connec[ind_cell  , : ] = [0, 0, (i+1)*nn-1  , (i+2)*nn-1 ]
 
 disp_mat = np.array([ [          0, 0   , 0, 0   ],
-                      [   nelems/2, L*.5, 0, L*.1],
-                      [     nelems, L   , 0, 0   ]])
+                      [   nn-1, 0, 0, 0],
+                      [   nn*(nn-1), 0, 0, 0],
+                      [     nn*nn-1, 0   , 0, L   ]])
 
-fext_mat  = np.zeros((nelems-1,4))
-for i in range(nelems-1):
-    fext_mat[i,:] = [ i+1, 0, 0.0, -1.0e-3] # node fx fy fz
+print("nn", nn*nn-1)
+fext_mat = np.array([[ 1, 0, 0.0, -1.0e-3]]) # node fx fy fz
 
 nodes_def, normal_forces = solve( nodes, connec, youngs, areas, disp_mat, fext_mat )
 
