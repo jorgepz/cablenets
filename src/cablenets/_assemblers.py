1757cc7f8117ee0471c67f84970e12701922d8cc
jorgepz <jorge.jmpz@gmail.com>
2025-02-20 16:17:18 -0300

    separate functions in file

---
+++ src/cablenets/_assemblers.py
@@ -0,0 +1,83 @@
+
+from cvxopt import matrix, spmatrix, spdiag, solvers
+import numpy             as np
+
+
+def _assemble_B(nodes, connec, I ):
+    connec = connec[:,2:4]
+    nnodes = np.size( nodes, 0 )
+    nelem  = np.size( connec, 0 )
+    vals = []
+    Is   = []
+    Js   = []
+    print("assembling matrix B ...")
+    for ele in range( nelem ):
+        print(ele)
+        ini_node, end_node = connec[ele, :]
+        Is.extend( range( ele*3     +0, (ele+1)*3      ) )
+        Js.extend( range( ini_node*3+0, (ini_node+1)*3 ) )
+        vals.extend( (-1,-1,-1) )
+        
+        Is.extend( range( ele*3     +0, (ele+1)*3      ) )
+        Js.extend( range( end_node*3+0, (end_node+1)*3 ) )
+        vals.extend( (1,1,1) )
+    print("done.\n")
+    return spmatrix( vals, Is, Js, (3*nelem, 3*nnodes))
+
+#
+# input nparray matrix
+#
+def _assemble_d_or_p_vec( mat ):
+    n_nodes = np.size( mat, 0 )
+    vec  = []
+    dofs = []
+    # assemble
+    for i in range( n_nodes ):
+        this_node_dofs = list( range( int(mat[i,0]*3), int((mat[i,0]+1)*3) ) )
+        dofs.extend( this_node_dofs )
+        vec.extend( mat[i,1:] )
+    vec  = np.array( vec  )
+    dofs = np.array( dofs, dtype=int )
+    return vec, dofs
+
+def _assemble_G(n_dofs_d, nnodes, nelem ):
+    vals = []
+    Is   = []
+    Js   = []
+    print("assembling matrix G ...")
+    for ele in range( nelem ):
+        # q
+        Is.extend( [ (ele)*4 ] )
+        Js.extend( [  ele    ] )
+        vals.extend( [-1] )
+        
+        # v
+        Is.extend( range(       ele*4+1,        ele*4+4 ) )
+        Js.extend( range( nelem+ele*3  , nelem+(ele+1)*3      ) )
+        vals.extend( (-1.0,-1.0,-1.0) )
+    print(" done.\n")
+    return spmatrix( vals, Is, Js, ((1+3)*nelem, (1+3)*nelem + n_dofs_d))
+
+def _assemble_P_and_q(nodes, connec, youngs, areas, n_dofs_d, nelem, d_vec ):
+    youngs = youngs[ connec[:,0]]
+    areas  = areas [ connec[:,1]]
+    connec = connec[:,2:4]
+
+    vals = []
+    Is   = []
+    Js   = []
+    lengths = np.empty((nelem))
+
+    print("assembling matrix P and q ...")
+    for ele in range( nelem ):
+        # q
+        Is.extend(   [ ele ] )
+        Js.extend(   [ ele ] )
+        lengths[ele] = np.linalg.norm( nodes[ connec[ele,1],:] - nodes[ connec[ele,0],:])
+        k = youngs[ele]*areas[ele]/lengths[ele]
+        vals.extend( [1/k] )
+    print(" done.\n")
+    P = spmatrix( vals, Is, Js, ((1+3)*nelem+n_dofs_d, (1+3)*nelem+n_dofs_d) )
+    q = matrix( [ [matrix(lengths).trans()], [matrix(0.0,(1,3*nelem))], [ matrix( -np.array(d_vec) ).trans() ] ] ).trans()
+    print("P", P)
+    return P, q
--- src/cablenets/cablenets.py
+++ src/cablenets/cablenets.py
@@ -31,86 +31,7 @@ import matplotlib.colors as colo
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-# input nparray matrices
-def _assemble_B(nodes, connec, I ):
-    connec = connec[:,2:4]
-    nnodes = np.size( nodes, 0 )
-    nelem  = np.size( connec, 0 )
-    vals = []
-    Is   = []
-    Js   = []
-    print("assembling matrix B ...")
-    for ele in range( nelem ):
-        print(ele)
-        ini_node, end_node = connec[ele, :]
-        Is.extend( range( ele*3     +0, (ele+1)*3      ) )
-        Js.extend( range( ini_node*3+0, (ini_node+1)*3 ) )
-        vals.extend( (-1,-1,-1) )
-        
-        Is.extend( range( ele*3     +0, (ele+1)*3      ) )
-        Js.extend( range( end_node*3+0, (end_node+1)*3 ) )
-        vals.extend( (1,1,1) )
-    print("done.\n")
-    return spmatrix( vals, Is, Js, (3*nelem, 3*nnodes))
-
-#
-# input nparray matrix
-#
-def _assemble_d_or_p_vec( mat ):
-    n_nodes = np.size( mat, 0 )
-    vec  = []
-    dofs = []
-    # assemble
-    for i in range( n_nodes ):
-        this_node_dofs = list( range( int(mat[i,0]*3), int((mat[i,0]+1)*3) ) )
-        dofs.extend( this_node_dofs )
-        vec.extend( mat[i,1:] )
-    vec  = np.array( vec  )
-    dofs = np.array( dofs, dtype=int )
-    return vec, dofs
-
-def _assemble_G(n_dofs_d, nnodes, nelem ):
-    vals = []
-    Is   = []
-    Js   = []
-    print("assembling matrix G ...")
-    for ele in range( nelem ):
-        # q
-        Is.extend( [ (ele)*4 ] )
-        Js.extend( [  ele    ] )
-        vals.extend( [-1] )
-        
-        # v
-        Is.extend( range(       ele*4+1,        ele*4+4 ) )
-        Js.extend( range( nelem+ele*3  , nelem+(ele+1)*3      ) )
-        vals.extend( (-1.0,-1.0,-1.0) )
-    print(" done.\n")
-    return spmatrix( vals, Is, Js, ((1+3)*nelem, (1+3)*nelem + n_dofs_d))
-
-def _assemble_P_and_q(nodes, connec, youngs, areas, n_dofs_d, nelem, d_vec ):
-    youngs = youngs[ connec[:,0]]
-    areas  = areas [ connec[:,1]]
-    connec = connec[:,2:4]
-
-    vals = []
-    Is   = []
-    Js   = []
-    lengths = np.empty((nelem))
-
-    print("assembling matrix P and q ...")
-    for ele in range( nelem ):
-        # q
-        Is.extend(   [ ele ] )
-        Js.extend(   [ ele ] )
-        lengths[ele] = np.linalg.norm( nodes[ connec[ele,1],:] - nodes[ connec[ele,0],:])
-        k = youngs[ele]*areas[ele]/lengths[ele]
-        vals.extend( [1/k] )
-    print(" done.\n")
-    P = spmatrix( vals, Is, Js, ((1+3)*nelem+n_dofs_d, (1+3)*nelem+n_dofs_d) )
-    q = matrix( [ [matrix(lengths).trans()], [matrix(0.0,(1,3*nelem))], [ matrix( -np.array(d_vec) ).trans() ] ] ).trans()
-    print("P", P)
-    return P, q
-
+from cablenets._assemblers import _assemble_B, _assemble_d_or_p_vec, _assemble_G, _assemble_P_and_q
 
 def _remove_loads_in_fixed_nodes(p_vec, dofs_p, d_vec, dofs_d):
     filtered_dofs_p = dofs_p
@@ -127,7 +48,7 @@ def _remove_loads_in_fixed_nodes(p_vec, dofs_p, d_vec, dofs_d):
 #
 # variables are x: [q,v,r] and s
 #
-def solve( nodes, connec, youngs, areas, disp_mat, fext_mat ):
+def solve( nodes, connec, youngs, areas, def_coord_mat, fext_mat, primal_dual = "primal"  ):
 
     print( "\n=== Welcome to cablenets ===\n" )
     nnodes = np.size( nodes, 0 )
@@ -138,7 +59,7 @@ def solve( nodes, connec, youngs, areas, disp_mat, fext_mat ):
     B = _assemble_B(nodes, connec, I)    
 
     # assemble d and p
-    d_vec, dofs_d = _assemble_d_or_p_vec(disp_mat)    
+    d_vec, dofs_d = _assemble_d_or_p_vec(def_coord_mat)    
     p_vec_aux, dofs_p_aux = _assemble_d_or_p_vec(fext_mat)    
 
     p_vec  = np.zeros( 3*nnodes )
@@ -178,13 +99,14 @@ def solve( nodes, connec, youngs, areas, disp_mat, fext_mat ):
     cvxG = _assemble_G( n_dofs_d, nnodes, nelem )
     cvxh = matrix(0.0,(1,(1+3)*nelem)).trans()
 
-    # primal equality constraints
+    # primary vars equality constraints
     cvxA = matrix([
     [ spmatrix([],[],[], (n_dofs_p, nelem )), spmatrix([],[],[], (n_dofs_d, nelem )) ],
     [ BTp, BTd ],
     [ spmatrix( [],[],[], (n_dofs_p, n_dofs_d )), -spdiag( matrix(1.0, (1,n_dofs_d)) ) ]
     ]) 
     cvxb = matrix( [ matrix( np.array(p_vec) ) , matrix(0.0, (n_dofs_d, 1)) ] )
+
 
     # cone set
     cvxdims = {'l': 0, 'q': [4]*nelem , 's': []}
